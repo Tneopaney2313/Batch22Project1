f0c9159ab8a509c82a0c5c3560bc0bebe3093e62
Tneopaney2313 <tneopaney2313@gmail.com>
2025-02-18 07:59:41 -0500

    first commit

--- src/BATCH22Project1/DublicateArray.java
+++ src/BATCH22Project1/DublicateArray.java
@@ -5,7 +5,7 @@ public class DublicateArray {
 
         int [] original= {1, 2, 3, 4, 5, 6,7};
         int [] duplicate=new int[original.length];
-        for (int i=0; i<original.length; i++){
+        for (int i=1; i<original.length; i++){
 
             duplicate[i]=original[i];
             System.out.println(i);
--- src/BATCH22Project1/secondLargestNumber.java
+++ src/BATCH22Project1/secondLargestNumber.java
@@ -1,16 +1,21 @@
 package BATCH22Project1;
 
+import java.lang.reflect.Array;
+
 public class secondLargestNumber {
     public static void main(String[] args) {
 
         int[] num = {12, 92, 17, 68, 23, 30, 37, 79, 7};
+
         int largest = num[0];
         int secondLargest = num[0];
 
         for (int n : num) {
-            if (n > largest) {
+            if (n < largest) {
                 secondLargest = largest;
                 largest = n;
+
+                System.out.println(n);
 
             }
            /* if (n < min){
